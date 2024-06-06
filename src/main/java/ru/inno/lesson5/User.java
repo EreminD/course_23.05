f5684ed974fe777340524670a56772c93eb9c515
Dmitry Eremin <mitya1705@gmail.com>
2024-06-06 23:05:28 +0300

    lesson 5

---
+++ src/main/java/ru/inno/lesson5/User.java
@@ -0,0 +1,25 @@
+package ru.inno.lesson5;
+
+public class User {
+    String name;
+    int age;
+    String phone;
+    String email;
+    double rating;
+    boolean isOnline;
+
+    public User(String myName, int myAge, boolean status, String myPhone, String myEmail) {
+        age = myAge;
+        name = myName;
+        phone = myPhone;
+        email = myEmail;
+        isOnline = status;
+        rating = 5.0;
+    }
+
+    public void getOlder() {
+        System.out.println("Сегодня я стал старше");
+        age = age + 1;
+    }
+
+}
